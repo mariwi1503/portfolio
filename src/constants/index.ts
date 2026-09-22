b3a3d483dca4c44aae7617136086db1ee60ff0c9
ary <mariwi1503@gmail.com>
2026-09-22 11:21:29 +0700

    feat: change role in trimitra teknologi

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -42,7 +42,7 @@ export const experienceList = [
     logo: "/experience/tmt.png",
     isActive: false,
     company: "PT Trimitra Mandiri Teknologi",
-    role: "Lead Engineer Developer",
+    role: "Lead Engineer",
     job: "Parttime",
     start: "Feb 2026",
     end: "Present",
